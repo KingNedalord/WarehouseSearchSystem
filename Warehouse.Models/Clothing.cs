308bdf379ed93c7e4c0ef37ebdf4e0acc3e4f1ec
ling <ling>
2025-10-26 19:13:21 +0500

    init skeleton

---
+++ Warehouse.Models/Clothing.cs
@@ -0,0 +1,20 @@
+public enum ClothingType
+{
+    Top,
+    Bottom,
+    Fullbody,
+    Outerwear,
+    Accessory,
+}
+
+public class Clothing : Item
+{
+    private readonly ClothingType clothingType;
+
+    public ClothingType ClothingType => clothingType;
+    public Clothing(int id, string name, Size size, Gender gender, decimal price, int quantity, ClothingType clothingType)
+        : base(id, name, size, gender, price, quantity)
+    {
+        this.clothingType = clothingType;
+    }
+}
---
+++ Warehouse.Models/Footwear.cs
@@ -0,0 +1,18 @@
+public enum FootwearType
+{
+    Casual,
+    Formal,
+    Special,
+}
+
+public class Footwear : Item
+{
+    private readonly FootwearType footwearType;
+
+    public FootwearType FootwearType => footwearType;
+    public Footwear(int id, string name, Size size, Gender gender, decimal price, int quantity, FootwearType footwearType)
+        : base(id, name, size, gender, price, quantity)
+    {
+        this.footwearType = footwearType;
+    }
+}
