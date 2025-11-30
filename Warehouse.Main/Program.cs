a9224904fbe4cad20ea03051f6122117ad80f05d
ling <ling>
2025-11-30 17:19:12 +0500

    entrypoint moved

---
+++ Warehouse.Main/Program.cs
@@ -0,0 +1,38 @@
+﻿using Warehouse.Presentation;
+using Warehouse.DAO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+
+        // Use relative paths that match the repository layout when running from the project's output folder
+        var clothingCsv = Path.Combine(baseDir, "..", "..", "..", "Data", "clothing.csv");
+        var footwearCsv = Path.Combine(baseDir, "..", "..", "..", "Data", "footwear.csv");
+
+        try
+        {
+            ItemDaoFactory.Instance.ConfigureSource(new ClothingCsvSource(clothingCsv));
+            ItemDaoFactory.Instance.ConfigureSource(new FootwearCsvSource(footwearCsv));
+
+            var viewFactory = ViewFactory.Instance;
+            var view = viewFactory.CreateView();
+            view.Start();
+        }
+        catch (Exception ex)
+        {
+            try
+            {
+                var view = ViewFactory.Instance.CreateView();
+                view.Crash();
+            }
+            catch
+            {
+                // ignore
+            }
+
+            Console.Error.WriteLine($"Application failed to start: {ex.Message}");
+        }
+    }
+}
